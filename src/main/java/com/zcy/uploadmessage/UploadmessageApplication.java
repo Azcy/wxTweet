c827ecc3f8d43ce2701fe5947c4ec624a8cd9c78
zcy <511929544@qq.com>
2018-04-18 17:10:53 +0800

    Initial commit

---
+++ src/main/java/com/zcy/uploadmessage/UploadmessageApplication.java
@@ -0,0 +1,12 @@
+package com.zcy.uploadmessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadmessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadmessageApplication.class, args);
+	}
+}
---
+++ src/main/java/com/zcy/uploadmessage/controller/UploadController.java
@@ -0,0 +1,75 @@
+package com.zcy.uploadmessage.controller;
+
+import com.zcy.uploadmessage.entity.Person;
+import com.zcy.uploadmessage.util.FileUtil;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+
+public class UploadController {
+    //跳转到上传文件的页面
+    @RequestMapping(value="/gouploadimg", method = RequestMethod.GET)
+    public String goUploadImg() {
+        //跳转到 templates 目录下的 uploadimg.html
+        return "uploadimg";
+    }
+
+    //处理文件上传
+    @RequestMapping(value="/testuploadimg", method = RequestMethod.POST)
+    public @ResponseBody
+    String uploadImg(@RequestParam("file") MultipartFile file,
+                     HttpServletRequest request) {
+        String contentType = file.getContentType();
+        String fileName = file.getOriginalFilename();
+        /*System.out.println("fileName-->" + fileName);
+        System.out.println("getContentType-->" + contentType);*/
+        String filePath ="/home/yk-zcy/IDEAWorkSpace/uploadmessage/src/main/resources/upload/";
+        //String filePath = request.getSession().getServletContext().getRealPath("imgupload/");
+        try {
+            FileUtil.uploadFile(file.getBytes(), filePath, fileName);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        //返回json
+        return "uploadimg success";
+    }
+
+    @RequestMapping("/")
+    public String hello() {
+        return "hello,Spring boot!";
+    }
+
+
+    @RequestMapping(value = "/greeting")
+    public ModelAndView test(ModelAndView mv) {
+        mv.setViewName("/greeting");
+        mv.addObject("title","欢迎使用Thymeleaf!");
+        return mv;
+    }
+
+
+    @RequestMapping(value = "/index")
+    public String index(Model model)
+    {
+        Person single = new Person("hyj",21);
+        List<Person> people = new ArrayList<Person>();
+        Person p1 = new Person("dlp",21);
+        Person p2 = new Person("tq",21);
+        Person p3 = new Person("mk",21);
+        people.add(p1);
+        people.add(p2);
+        people.add(p3);
+        model.addAttribute("singlePerson",single);
+        model.addAttribute("people",people);
+        return "index";
+    }
+}
